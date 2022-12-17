a5ac1043bf3e6f1c895f032c0a4c292ffb3e16a1
alireza.ak <alireza-akef76@yahoo.com>
2022-12-17 17:10:43 +0330

    adding cdn, solved the style configuration problem

--- tf.js
+++ tf.js
@@ -1,17 +1,17 @@
 
 // Modal
 
-var modalEle = document.querySelector(".modal");
-var modalImage = document.querySelector(".modalImage");
-Array.from(document.querySelectorAll("#ImgThumbnail")).forEach(item => {
-   item.addEventListener("click", event => {
-      modalEle.style.display = "block";
-      modalImage.src = event.target.src;
-   });
-});
-document.querySelector(".btn-close").addEventListener("click", () => {
-   modalEle.style.display = "none";
-});
+// var modalEle = document.querySelector(".modal");
+// var modalImage = document.querySelector(".modalImage");
+// Array.from(document.querySelectorAll("#ImgThumbnail")).forEach(item => {
+//    item.addEventListener("click", event => {
+//       modalEle.style.display = "block";
+//       modalImage.src = event.target.src;
+//    });
+// });
+// document.querySelector(".btn-close").addEventListener("click", () => {
+//    modalEle.style.display = "none";
+// });
 
 
 
